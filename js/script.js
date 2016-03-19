928f8a36f27f5fa6e752370f3fe047b40d6330ff
Michael <me@michaelkulinski.com>
2016-03-18 17:04:10 -0700

    furnished

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 $(document).ready(function() {
     //collection of quotes
-    var quotes = [
-      "You slower than molasses",
-        "Learn how to swerve and werve",
-        "Started from the bottom now we here",
-        "I put them bricks on the boat",
-        "Realist shit I never wrote."
-    ];
+    var quotes = {
+        "artists": [
+            "Migos",
+            "Fetty",
+            "Drake",
+            "The Donald",
+            "Uknown"
+        ],
+        "lyrics": [
+            "You slower than molasses",
+            "Learn how to swerve and werve",
+            "Started from the bottom now we here",
+            "I put them bricks on the boat",
+            "Realist shit I never wrote."
+        ]
+    };
 
-    var randomNumber = Math.floor(Math.random() * 6);
-
-
-    $("h3").html(quotes[randomNumber]);
+    $("button").on("click", function() {
+        var randomNumber = Math.floor(Math.random() * 5);
+        $("p").html("- " +quotes.artists[randomNumber]);
+        $("h3").html( quotes.lyrics[randomNumber]);
+    });
 });
